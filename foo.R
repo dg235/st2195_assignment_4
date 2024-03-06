23054c8a635ae82f848f9d8168b855caef5062e4
Dorothea Grkovic <157990654+dg235@users.noreply.github.com>
2024-03-06 11:16:34 +0200

    Update foo.R

--- foo.R
+++ foo.R
@@ -1,17 +1,15 @@
-# Radius
-r <- 2
 # Function to compute the volume of a sphere with radius r
-volume <- function(r, rho) {
- 3/4*pi*r^2
+volume <- function(r) {
+ 4/3*pi*r^3
 }
-# Function to compute the volumes of the spheres with radius r, r^2 and 
-r^3 
+# Function to compute the volumes of the spheres with radius r, r^2 and r^3
 volume_vector <- function(r) {
- r <- 22
- for (r in 2:4){
- volume(r)
+ volumes <- c(volume(r), volume(r^2), volume(r^3))
+ return(volumes)
  }
 }
+# Radius
+r <- 2
 # Run volume_vector(r) and print the volumes of the spheres with radius 
-r, r^2 and r^3 
-volume_vector(r)
+volumes <- volume_vector(r)
+print(volumes)
